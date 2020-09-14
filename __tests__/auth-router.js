b5d0b12b94cde740075a1e0969de4b6da7170f93
Simone Roy <simonecarolynr@gmail.com>
2020-09-13 19:55:00 -0600

    testing g and questions still needs work

--- __tests__/auth-router.js
+++ __tests__/auth-router.js
@@ -2,14 +2,48 @@ const supertest = require("supertest")
 const server = require("../api/server")
 const db = require("../database/dbConfig")
 
+afterAll(async () => {
+    //close the database connection so the test process doesn't hang or give a warning
+    await db.destroy()
+})
+
+describe("Testing Login and Registration endpoints", () => {
 //test POST /login
-
+    it("POST /login, if there is no user found it returns an error", async () => {
+        const res = await supertest(server)
+         .post("/api/auth/login")
+         .send({ username: "randomUs3r" })
+        expect(res.statusCode).toBe(401)
+    })
 
 //test POST /login
-
+   it("POST /login, if the password input is wrong it returns an error", async () => {
+        const res = await supertest(server)
+         .post("/api/auth/login")
+         .send({ password: "randomPassw0rd" })
+        expect(res.statusCode).toBe(401)
+    })
 
 
 //test POST /register
-
+   it("POST /register, if the username already exists it returns an error", async () => {
+        const res = await supertest(server)
+         .post("/api/auth/register")
+         .send({ username: "simoneroy" })
+        expect(res.statusCode).toBe(409)
+    })
 
 //test POST /register
+   it("POST /register, if everything checks out it returns a success message", async () => {
+        const res = await supertest(server)
+         .post("/api/auth/register")
+         .send({
+             username: "testUser",
+             password: "testPassword"
+         })
+        expect(res.statusCode).toBe(201)
+        expect(res.type).toBe("application/json")
+        expect(res.body.username).toBe("testUser")
+    })
+})
+
--- __tests__/jokes-router.js
+++ __tests__/jokes-router.js
@@ -1,8 +1,21 @@
 const supertest = require("supertest")
 const server = require("../api/server")
 const db = require("../database/dbConfig")
+const { expectCt } = require("helmet")
+
+describe("Dad jokes tests", () => {
+//test GET /
+it("GET /, if the user is authenticated it will return an array", async () => {
+
+
+})
+
 
 //test GET /
+it("GET /, if the user is not authenticated, they will not be let in", async () => {
+    const res = await supertest(server)
+     .get("/api/jokes")
+    expect(res.statusCode).toBe(401)
 
-
-//test GET /
+})
+})
